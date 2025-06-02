d24e6133ca37ec9627d4a18d696e46990d4e8fef
itzelescuderoj <20243ds083@utez.edu.mx>
2025-06-02 09:45:28 -0600

    Figuras geometricas agragando triangulos

--- U2_TI_01_Figuras/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module mx.edu.utez.u2_ti_01_figuras {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens mx.edu.utez.u2_ti_01_figuras to javafx.fxml;
-    exports mx.edu.utez.u2_ti_01_figuras;
-}
